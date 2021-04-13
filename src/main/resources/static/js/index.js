f1e251964c11fcd9703086b15c5461487439378a
Anthony Biletsky <abiletsky@rhythmedix.com>
2021-04-12 20:24:53 -0400

    UI changes

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,31 +1,36 @@
-var d = document.getElementById('movingDiv');
+var timerRate = 1;
 
-var degrees = 300;
-
-d.style.position = "absolute";
-d.innerText = d.getAttribute('data-fruit-name');
-var rect = d.getBoundingClientRect();
-
-var posX = rect.left + window.scrollX;
-var posY = rect.top + window.scrollY;
-
-var id = clearInterval(id);
-id = setInterval(frame, 500);
-
-// Calculate slope from angle and one point: https://stackoverflow.com/questions/1571294/line-equation-with-angle
-// Don't need slope, calc new x and new y instead: https://math.stackexchange.com/questions/143932/calculate-point-given-x-y-angle-and-distance
-  function frame() {
-    if (posX >= 350) {
-      clearInterval(id);
-    } else {
-      posX += d.clientWidth * (Math.cos(degrees * Math.PI / 180));
-      posY += d.clientHeight * Math.sin(degrees * Math.PI / 180) * -1;
-      d.style.top = posY + "px";
-      d.style.left = posX + "px";
-    }
+  function changeRate() {
+    timerRate = document.getElementById('speed').value;
   }
 
-  function updateAngle() {
+  function startSimulation() {
+    var flights = document.getElementsByClassName("flight");
 
+    for(var i = 0; i < flights.length; i++) {
+        flights[i].style.position = "absolute";
+
+        var degrees = flights[i].getAttribute('data-heading');
+        var rect = flights[i].getBoundingClientRect();
+
+        var posX = rect.left + window.scrollX;
+        var posY = rect.top + window.scrollY;
+
+        var id = clearInterval(id);
+        id = setInterval(frame, 750);
+
+        // Calculate slope from angle and one point: https://stackoverflow.com/questions/1571294/line-equation-with-angle
+        // Don't need slope, calc new x and new y instead: https://math.stackexchange.com/questions/143932/calculate-point-given-x-y-angle-and-distance
+          function frame() {
+            if (posX != 350) {
+              clearInterval(id);
+            } else {
+              posX += flights[i].clientWidth * (Math.cos(degrees * Math.PI / 180));
+              posY += flights[i].clientHeight * Math.sin(degrees * Math.PI / 180) * -1;
+              flights[i].style.top = posY + "px";
+              flights[i].style.left = posX + "px";
+            }
+          }
+    }
   }
 
